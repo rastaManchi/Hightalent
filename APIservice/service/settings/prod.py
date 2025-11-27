8e98ab92264663ca97026c8e7a23b9a2d3e42132
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-11-28 00:26:04 +0300

    Docker fix

--- APIservice/service/settings/prod.py
+++ APIservice/service/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
